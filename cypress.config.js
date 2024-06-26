6add3dae6e80d71a82c4ffd620dc49752e2beee3
Rafail Ferre <qa.rafailferre@gmail.com>
2024-06-26 19:38:58 -0400

    install dotenv, add cypress.env, modified tests and files

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,24 @@
 const { defineConfig } = require("cypress");
+// const dotenv = require("dotenv").config();
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://clientbase.pasv.us/v6",
+    // env: {
+    //   baseUrl: process.env.BASE_URL,
+    //   email: process.env.EMAIL,
+    //   password: process.env.PASSWORD,
+    // },
+
     watchForFileChanges: false,
     excludeSpecPattern: [
-      'cypress/e2e/1-getting-started/*',
-      'cypress/e2e/2-advanced-examples/*',
-  ],
+      "cypress/e2e/1-getting-started/*",
+      "cypress/e2e/2-advanced-examples/*",
+    ],
+
+    // viewportWidth: 1200,
+    // viewportHeight: 700,
+
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
     // },
